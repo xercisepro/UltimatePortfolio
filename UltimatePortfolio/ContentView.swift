87895d7711e3a0e4a801eedebf7f366f125e704a
Andrew Markham <andrew@xercisepro.com>
2020-11-06 17:23:21 +1100

    Between app ussage implemented

--- UltimatePortfolio/ContentView.swift
+++ UltimatePortfolio/ContentView.swift
@@ -8,21 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @SceneStorage("selectedView") var selectedView: String?//Optional String so make sure the property is too
+    //additonally for this make sure you are supporting scenes by using @SceneStorage instead of @appStorage
+    
     var body: some View {
-        TabView{
+        TabView(selection: $selectedView){
             HomeView()
+                .tag(HomeView.tag)
                 .tabItem {
                     Image(systemName: "house")
                     Text("Home")
                 }
         
             ProjectView(showClosedProjects: false)
+                .tag(ProjectView.openTag)
                 .tabItem {
                     Image(systemName: "list.bullet")
                     Text("Open")
                 }
         
             ProjectView(showClosedProjects: true)
+                .tag(ProjectView.closedTag)
                 .tabItem {
                     Image(systemName: "checkmark")
                     Text("Closed")
