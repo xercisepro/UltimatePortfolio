cd3667ee3838181efdf821ad04b76014efca36aa
Andrew Markham <andrew@xercisepro.com>
2020-11-06 16:58:27 +1100

    initial setup

---
+++ UltimatePortfolio/Item-CoreDataHelpers.swift
@@ -0,0 +1,36 @@
+//
+//  Item-CoreDataHelpers.swift
+//  UltimatePortfolio
+//
+//  Created by Andrew CP Markham on 6/11/20.
+//
+
+import Foundation
+
+//Used to support optional values
+extension Item{
+    var itemTitle: String{
+        title ?? ""
+    }
+    
+    var itemDetail: String{
+        detail ?? ""
+    }
+    
+    var itemCreationDate: Date{
+        creationDate ?? Date()
+    }
+    
+    static var example: Item{
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let item = Item(context: viewContext)
+        item.title = "Example Item"
+        item.detail = "This is an example item"
+        item.priority = 3
+        item.creationDate = Date()
+        return item
+    }
+}
+
