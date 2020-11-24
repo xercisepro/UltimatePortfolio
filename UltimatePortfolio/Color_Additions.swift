3a1179c3b8a61e3a8e85eba80880bba6afd99195
Andrew Markham <andrew@xercisepro.com>
2020-11-24 14:56:55 +1100

    Implemented home screen as dashboard

---
+++ UltimatePortfolio/Color_Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color_Additions.swift
+//  UltimatePortfolio
+//
+//  Created by Andrew CP Markham on 24/11/20.
+//
+
+import SwiftUI
+
+extension Color{
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
