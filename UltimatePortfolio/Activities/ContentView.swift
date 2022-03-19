8c9cf2b37dbdaf27cf2133c80e38a68254cc4111
Andrew Markham <andrew.markham@iinet.net.au>
2022-03-20 08:09:37 +1100

    Added Shortcut Feature - to add new project

--- UltimatePortfolio/Activities/ContentView.swift
+++ UltimatePortfolio/Activities/ContentView.swift
@@ -12,6 +12,10 @@ struct ContentView: View {
     @SceneStorage("selectedView") var selectedView: String?// Optional String so make sure the property is too
     // additonally for this make sure you are supporting scenes by using @SceneStorage instead of @appStorage
     @EnvironmentObject var dataController: DataController
+
+    // shortcut activity magic key
+    private let newProjectActivity = "com.xercisepro.newProject"
+
     var body: some View {
         TabView(selection: $selectedView) {
             HomeView(dataController: dataController)
@@ -39,7 +43,13 @@ struct ContentView: View {
                     Text("Awards")
                 }
         }
-        .onContinueUserActivity(CSSearchableItemActionType, perform: moveToHome)
+        .onContinueUserActivity(CSSearchableItemActionType, perform: moveToHome) // spotlight activity
+        .onContinueUserActivity(newProjectActivity, perform: createProject) // shortcut activity
+        .userActivity(newProjectActivity, { activity in
+            activity.isEligibleForPrediction = true // this allows the OS prompt this based on user behaviour
+            activity.title = "New Project"
+        })
+
         .onOpenURL(perform: openURL)
     }
 
@@ -49,7 +59,13 @@ struct ContentView: View {
 
     func openURL(_ url: URL) {
         selectedView = ProjectView.openTag
-        _ = dataController.addProject()
+        dataController.addProject()
+    }
+
+    func createProject(_ userActivity: NSUserActivity) {
+        // Function that is called by the OS shortcut feature for the app
+        selectedView = ProjectView.openTag
+        dataController.addProject()
     }
 }
 
